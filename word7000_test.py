7fe3aa0a0a5f41365b2f46e8ab3d73c8214ba9b2
Kevin18Chen <103588198+Kevin18Chen@users.noreply.github.com>
2023-10-01 20:16:14 +0800

    Add files via upload

--- word7000_test.py
+++ word7000_test.py
@@ -13,23 +13,31 @@ word = list(csv.reader(f, delimiter=','))
 wordlen = len(word)
 f.close()
 
-rannum = 100
+rannum = 140
+testnum = 10
 
+def settung():
+    global rannum,testnum,wordlen
+    print('總題數：' + str(wordlen))
+    rannum = int(input('答題範圍(題號到最後一題)：'))
+    testnum = int(input('作答題數：'))
+    
 def Pinyin(fun):
-    global word,wordlen,rannum
+    global word,wordlen,rannum,testnum
     num = 0
     error = []
     topic = []
     yans = []
     if(fun == 1):
         engine = pyttsx3.init()
-    for i in range(10):
+    for i in range(testnum):
         num = randint(rannum+1,wordlen-1)
         try:
             while(topic.index(num) >= 0):
                 num = randint(rannum+1,wordlen-1)
         except:
             topic.append(num)
+        #print('答對：{0:<10}答錯：{1:<10}'.format(testnum-len(error),len(error)))
         print(str(i + 1) + '：' + str(word[num][1]) + '\t' + str(word[num][2]))
         if(fun == 1):
             engine.say(word[num][0])
@@ -45,7 +53,7 @@ def Pinyin(fun):
     print('答錯' + str(len(error)) + '題')
 
 def choose(fun):
-    global word,wordlen,rannum
+    global word,wordlen,rannum,testnum
     num = 0
     cosn = 0
     ansn = 0
@@ -53,7 +61,7 @@ def choose(fun):
     topic = []
     if(fun == 1):
         engine = pyttsx3.init()
-    for i in range(10):
+    for i in range(testnum):
         Options = {'1':'','2':'','3':'','4':''}
         Dreg = [1,2,3,4]
         num = randint(rannum+1,wordlen-1)
@@ -63,6 +71,7 @@ def choose(fun):
                 topic.index(num)
         except:
             topic.append(num)
+        print('答對：{0:<10}答錯：{1:<10}'.format(i-len(error),len(error)))
         print(str(i + 1) + '：' + str(word[num][0]) + '\t' + str(word[num][2]))
         cosn = Dreg[randint(0,len(Dreg)-1)]
         ansn = cosn
@@ -91,7 +100,7 @@ def choose(fun):
     
 def test():
     while(1):
-        mod = int(input('1.拼字無聲, 2.拼字有聲, 3.選字無聲, 4.選字有聲：'))
+        mod = int(input('1.拼字無聲, 2.拼字有聲, 3.選字無聲, 4.選字有聲, 5.設定：'))
         os.system('cls')
         if(mod == 1):
             Pinyin(0)
@@ -101,6 +110,8 @@ def test():
             choose(0)
         elif(mod == 4):
             choose(1)
+        elif(mod == 5):
+            settung()
         else:
             break;
 test()
